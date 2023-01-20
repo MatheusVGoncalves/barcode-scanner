bda43e82a6c631e00f7a831321ac467aaa37ed91
MatheusVGoncalves <math_negocios@hotmail.com>
2023-01-20 14:32:59 -0300

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, Modal, StyleSheet, Text, View } from "react-native";
+
+import { Scanner } from "./src/components/Scanner";
 
 export default function App() {
+  const [modalVisible, setModalVisible] = React.useState(false);
+
+  const [type, setType] = React.useState("");
+  const [data, setData] = React.useState("");
+
+  const onCodeScanned = (type: string, data: string) => {
+    setType(type);
+    setData(data);
+    setModalVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modal}>
+          <Scanner onCodeScanned={onCodeScanned} />
+          <Button title="Cancelar" onPress={() => setModalVisible(false)} />
+        </View>
+      </Modal>
+
       <StatusBar style="auto" />
+      <View style={{ flex: 0.6, alignItems: "center" }}>
+        <Text>
+          Tipo de Leitura:{" "}
+          {Number(type) === 128
+            ? "Código de Barras"
+            : Number(type) === 256
+            ? "QRcode"
+            : type !== ""
+            ? "Outro"
+            : ""}
+        </Text>
+        <Text>Dados gerados: </Text>
+        <Text>{data}</Text>
+      </View>
+
+      <Button title="Escanear" onPress={() => setModalVisible(true)} />
     </View>
   );
 }
@@ -13,8 +54,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modal: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "lightgrey",
   },
 });
---
+++ src/components/Scanner/index.tsx
@@ -0,0 +1,55 @@
+import { BarCodeScanner } from "expo-barcode-scanner";
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+type Props = {
+  onCodeScanned: (type: string, data: string) => void;
+};
+
+export function Scanner(props: Props) {
+  const [hasPermission, setHasPermission] = useState(false);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }: any) => {
+    setScanned(true);
+    props.onCodeScanned(type, data);
+    //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "90%",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+      }}
+    >
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+
+      {scanned && (
+        <Button
+          title={"Repetir Escaneamento"}
+          onPress={() => setScanned(false)}
+        />
+      )}
+    </View>
+  );
+}
